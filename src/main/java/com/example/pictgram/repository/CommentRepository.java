09f1545e499fb660cf45ba205de59d91dc9dd8e1
kazuki <w124585421@yahoo.co.jp>
2022-03-27 23:58:13 +0900

    update #9

--- src/main/java/com/example/pictgram/repository/CommentRepository.java
+++ src/main/java/com/example/pictgram/repository/CommentRepository.java
@@ -3,10 +3,9 @@ package com.example.pictgram.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.pictgram.entity.User;
+import com.example.pictgram.entity.Comment;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 	
-	User findByUsername(String username);
 }
